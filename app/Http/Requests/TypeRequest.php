f01092f4b73cd29b7f045827c65ef077e0f07ce1
cy <jercyguevarra07@gmail.com>
2025-03-17 13:46:29 +0800

    wala lang (pic &regex for type)

--- app/Http/Requests/TypeRequest.php
+++ app/Http/Requests/TypeRequest.php
@@ -24,7 +24,7 @@ class TypeRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required','max:255']
+            'name' => ['required','regex:/^[a-zA-Z\s]+$/']
         ];
     }
     protected function failedValidation(Validator $validator){
