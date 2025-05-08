01a2e20b9cd92a3e452f7bbd2b54832a53ff1b74
Dexter Jamero <phlwin722@gmail.com>
2025-05-08 14:35:09 +0800

    dexter

--- quasar-project/src/boot/axios.js
+++ quasar-project/src/boot/axios.js
@@ -3,5 +3,5 @@ import axios from "axios";
 axios.defaults.baseURL =
   process.env.NODE_ENV === "production"
     ? window.location.origin
-    : "http://127.0.0.1:8002";
+    : "http://192.168.0.153:8002";
 export { axios };
